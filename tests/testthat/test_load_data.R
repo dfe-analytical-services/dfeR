fa0bc58c1e10f01425e9ec7f096ef8d3dda84cc3
Adam Robinson <adamrobinson361@gmail.com>
2018-03-13 16:47:34 +0000

    Added unit tests for sql_conn_string
    1. Test that two basic strings work
    2. Test for example database
    3. Tests for graceful errors if non character vectors provided.

---
+++ tests/testthat/test_load_data.R
@@ -0,0 +1,18 @@
+context("sql_conn_string")
+
+test_that("Basic test case works", {
+  expect(is.character(sql_conn_string("database","server")), "Output not a string")
+})
+
+test_that("Example Database as Expected", {
+  expect_equal(sql_conn_string("3DCPRI-PDB16\\ACSQLS", "SWFC_Project"),
+  "driver={SQL Server};server=3DCPRI-PDB16\\ACSQLS;database=SWFC_Project;trusted_connection=TRUE")
+})
+
+test_that("Deals with non character server variable gracefully", {
+  expect_error(sql_conn_string(1, "database"), "server parmaeter must be of type character")
+})
+
+test_that("Deals with non character database variable gracefully", {
+  expect_error(sql_conn_string("server", data.frame(a=1)), "database parmaeter must be of type character")
+})
