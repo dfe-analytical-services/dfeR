ee460f0c785ff90fc19f7f207cc4d145d5671cc4
Laura Selby <laura.selby@hotmail.co.uk>
2018-04-15 12:50:45 +0100

    restrict to numbers only and added @examples

--- R/formatting.R
+++ R/formatting.R
@@ -6,14 +6,13 @@
 #' @param year Academic year
 #' @return A formatted academic year
 #' @export
+#' @examples
+#' format_ay(20161)
 format_ay <- function(year){
 
-  if (!is.numeric(year)) stop("year parameter must be a number e.g. 201617")
-
-  if (nchar(year)!=6) stop("year parameter should be 6 digits long e.g. 201617")
+  if (!grepl("^[0-9]{6,6}$",year)) stop("year parameter must be a six didgit number e.g. 201617")
 
   sub("(.{4})(.*)", "\\1/\\2", year)
 
 }
-
 
--- tests/testthat/test_formatting.R
+++ tests/testthat/test_formatting.R
@@ -1,10 +1,13 @@
 context("format_ay")
 
 test_that("Deals with non numeric year variable gracefully", {
-  expect_error(format_ay("year"), "year parameter must be a number e.g. 201617")
+  expect_error(format_ay("year"), "year parameter must be a six didgit number e.g. 201617")
 })
 
 test_that("Input as expected", {
-  expect_error(format_ay(20102011), "year parameter should be 6 digits long e.g. 201617")
+  expect_error(format_ay(20102011), "year parameter must be a six didgit number e.g. 201617")
 })
 
+test_that("Input as expected", {
+  expect_error(format_ay(2011), "year parameter must be a six didgit number e.g. 201617")
+})
