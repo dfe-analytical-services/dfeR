6d4e74d599189e024dfe2d0ca7904b0b6b16c4d7
Laura Selby <laura.selby@hotmail.co.uk>
2018-05-16 14:40:46 +0100

    Added round_nearest function

--- tests/testthat/test_formatting.R
+++ tests/testthat/test_formatting.R
@@ -19,3 +19,21 @@ test_that("Example numerical known answer", {
 test_that("Example character known answer", {
   expect_equal(format_ay("201112"), "2011/12")
 })
+
+context("round_nearest")
+
+test_that("Input as expected", {
+  expect_error(round_nearest(12, "ten"), "n must be a number")
+})
+
+test_that("Input as expected", {
+  expect_error(round_nearest("twelve", 10), "x must be a number")
+})
+
+test_that("Example numerical known answer", {
+  expect_equal(round_nearest(1337,5), "1335")
+})
+
+test_that("Example character known answer", {
+  expect_equal(round_nearest("1337",10), "1340")
+})
