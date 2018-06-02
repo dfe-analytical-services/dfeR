76b73bbf9d0895615d9f45b03e1ec9861c15364a
Adam Robinson <adamrobinson361@gmail.com>
2018-06-02 20:28:37 +0000

    Added national statistic rmarkdown formatting and removed yaml dependency (test)

--- R/styles.R
+++ R/styles.R
@@ -1,14 +1,24 @@
-#' Test
+#' R Markdown DfE National Statistic Output Format
 #'
+#' This function creates an R Markdown output format that generates documents
+#' in the style of an DfE National Statistics Publications. \cr
+#'
+#' To format an R Markdown document using this template add the function to the R Markdowns
+#' YAML header as follows:
+#'
+#' title: "Name" \cr
+#' output: dfeR::national_statistic
+#'
+#' @param ... All Parameters from rmarkdown::word_document are accepted
 #' @export
+#' @examples
+#' national_statistic()
 
-
-test_style <- function(toc = TRUE) {
+national_statistic <- function(...) {
 
   # get the locations of resource files located within the package
-  tmp <- system.file("styles/template.docx", package = "dfeR")
+  file <- system.file("styles/national_statistic.docx", package = "dfeR")
 
   # call the base html_document function
-  rmarkdown::word_document(toc = toc,
-                           reference_docx = tmp)
+  rmarkdown::word_document(reference_docx = file)
 }
