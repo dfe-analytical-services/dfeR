478410c0c403c0a7b0a8e8411356772aae97fb00
Adam Robinson <adamrobinson361@gmail.com>
2019-06-20 19:26:22 +0100

    Misc changes

--- R/proxy.R
+++ R/proxy.R
@@ -1,6 +1,6 @@
 #' Sets up Environment Variables to authenticate with the DfE Proxy for working with web content.
 #'
-#' This function sets up User environment variables http_proxy and https_proxy so that R (and other software) can access web conent. E.g. install packages from github, web scrape etc.
+#' This function sets up user environment variables http_proxy and https_proxy so that R (and other software) can access web conent. E.g. install packages from github, web scrape etc.
 #'
 #' This function will need to be re-run every time you change your windows password.
 #'
@@ -11,10 +11,10 @@
 #' }
 
 setup_proxy <- function(){
-  
+
   # Ask the user for their password
   password <- rstudioapi::askForPassword("Please provide your Windows Password to authenticate.")
-  
+
   # Construct proxy url using username and password
   proxy_encoded_url <- URLencode(
     paste0(
@@ -24,13 +24,13 @@ setup_proxy <- function(){
       password,
       "@192.168.2.40:8080"
     ))
-  
+
   # Construct commands to create environment variables
   http_proxy_cmd <-  paste0("setx http_proxy ", proxy_encoded_url)
   https_proxy_cmd <-  paste0("setx https_proxy ", proxy_encoded_url)
-  
+
   # Create system environment variables
   system(http_proxy_cmd)
   system(https_proxy_cmd)
-  
+
 }
