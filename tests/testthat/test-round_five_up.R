f9b012e4b9d694db69ccdfccbecebd9aa0ace22c
Cam Race <52536248+cjrace@users.noreply.github.com>
2024-02-12 13:08:24 +0000

    Add default value to round five up (#62)
    * add extra examples and default dp value
    
    * update error messages to be more specific
    
    * fix typo in round_five_up() documentation
    
    * style round_five_up() code
    
    * change argument from value to number
    
    * Tweak r5u wording based on PR comments to be easier to understand

--- tests/testthat/test-round_five_up.R
+++ tests/testthat/test-round_five_up.R
@@ -1,32 +1,38 @@
 test_that("Rounds fives up", {
   expect_equal(round_five_up(285, -1), 290)
+  expect_equal(round_five_up(2.85), 3)
+  expect_equal(round_five_up(2.5), 3)
   expect_equal(round_five_up(2.85, 1), 2.9)
 })
 
 test_that("Rounds other numbers", {
-  expect_equal(round_five_up(283, -1), 280)
-  expect_equal(round_five_up(2.87, 1), 2.9)
+  expect_equal(round_five_up(283.54, -2), 300)
+  expect_equal(round_five_up(283.54, -1), 280)
+  expect_equal(round_five_up(283.234, 0), 283)
+  expect_equal(round_five_up(283.234), 283)
+  expect_equal(round_five_up(2.86, 1), 2.9)
+  expect_equal(round_five_up(2.86, 2), 2.86)
 })
 
 test_that("Input validation", {
   expect_error(
     round_five_up("ten", "10"),
-    "both inputs must be numeric"
+    "both input arguments must be numeric"
   )
   expect_error(
     round_five_up(12, "ten"),
-    "the decimal places value must be numeric"
+    "the decimal places input must be numeric"
   )
   expect_error(
     round_five_up(12, "10"),
-    "the decimal places value must be numeric"
+    "the decimal places input must be numeric"
   )
   expect_error(
     round_five_up("twelve", 10),
-    "the value to be rounded must be numeric"
+    "the input number to be rounded must be numeric"
   )
   expect_error(
     round_five_up("12", 10),
-    "the value to be rounded must be numeric"
+    "the input number to be rounded must be numeric"
   )
 })
