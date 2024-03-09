e39ea6ab841405eb2246d30b376a64c80f2381fc
cjrace <camrace8@gmail.com>
2024-03-09 10:40:23 +0000

    remove capitalised extension test (causes test errors on linux)

--- tests/testthat/test-get_clean_sql.R
+++ tests/testthat/test-get_clean_sql.R
@@ -6,13 +6,6 @@ test_that("Can retrieve basic script", {
       " */  SELECT * FROM [my_database_table]"
     )
   )
-  expect_equal(
-    get_clean_sql("../sql_scripts/simple.SQL"),
-    paste0(
-      " /* Simple SQL script to get all data from my database table",
-      " */  SELECT * FROM [my_database_table]"
-    )
-  )
 })
 
 test_that("Adds additional settings", {
