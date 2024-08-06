a38d03a2e5b880e357a4a1a380f837188400bdfb
Jake Tufts <jake.tufts@education.gov.uk>
2024-08-06 21:10:03 +0100

    Using lintr

--- R/create_project.R
+++ R/create_project.R
@@ -46,16 +46,16 @@ create_project <- function(
     ...) {
   # Function parameter checks ---
   # Check if the parameters are 1 length booleans
-  if (!is.logical(init_renv) | length(init_renv) != 1) {
+  if (!is.logical(init_renv) || length(init_renv) != 1) {
     stop("init_renv must be a boolean.")
-  } else if (!is.logical(include_structure_for_pkg) |
-    length(include_structure_for_pkg) != 1) {
+  } else if (!is.logical(include_structure_for_pkg) ||
+               length(include_structure_for_pkg) != 1) {
     stop("include_structure_for_pkg must be a boolean.")
-  } else if (!is.logical(create_publication_proj) |
-    length(create_publication_proj) != 1) {
+  } else if (!is.logical(create_publication_proj) ||
+               length(create_publication_proj) != 1) {
     stop("create_publication_proj must be a boolean.")
-  } else if (!is.logical(include_github_gitignore) |
-    length(include_github_gitignore) != 1) {
+  } else if (!is.logical(include_github_gitignore) ||
+               length(include_github_gitignore) != 1) {
     stop("include_github_gitignore must be a boolean.")
   }
 
@@ -318,7 +318,7 @@ create_project <- function(
   if (!file.exists(paste0(path, "/.Rprofile"))) {
     file.create(paste0(path, "/.Rprofile"))
   }
-  rprofile_content <- script <- paste(
+  rprofile_content <- paste(
     ".First <- function() {",
     "if ('praise' %in% installed.packages()) {",
     "    message(praise::praise('${Exclamation}-${Exclamation}! '),",
