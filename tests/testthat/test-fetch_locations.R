e71d8d309d0b47ba7d545959b54a4f5d03b0d5e1
Cam Race <cameron.race@education.gov.uk>
2024-08-15 18:05:19 +0100

    add verbose toggle and general WIP

--- tests/testthat/test-fetch_locations.R
+++ tests/testthat/test-fetch_locations.R
@@ -1,31 +1,38 @@
 test_that("fetch_locations gives a data frame", {
   expect_true(is.data.frame(fetch_pcons()))
-  # expect_true(is.data.frame(fetch_lads()))
-  # expect_true(is.data.frame(fetch_las()))
+  expect_true(is.data.frame(fetch_lads()))
+  expect_true(is.data.frame(fetch_las()))
 })
 
 test_that("fetch_locations have no duplicate rows", {
   expect_true(!anyDuplicated(fetch_pcons()))
-  # expect_true(!anyDuplicated(fetch_lads()))
-  # expect_true(!anyDuplicated(fetch_las()))
+  expect_true(!anyDuplicated(fetch_lads()))
+  expect_true(!anyDuplicated(fetch_las()))
 })
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # These tests will fail if there are genuine changes to the locations
 # Counts of locations done manually from the data screener repo lookup we had
+# Used this kind of thing and counted the YES cells:
+# =IF(AND($A2<=2022, $B2>=2022), "YES", "NO")
+# =IF(OR(E2="YES", F2="YES"), "YES", "NO")
 test_that("year filtering works as expected", {
   # PCONs
-  expect_equal(nrow(fetch_pcons()), 678888888)
-
-  expect_equal(nrow(fetch_pcons(c("2024", "2023"))), 678888888)
+  expect_equal(nrow(fetch_pcons(c("2017", "2019", "2020", "2021", "2022", "2023", "2024"))), 1295)
+  # expect_equal(nrow(fetch_pcons(c("2022", "2023"))), 650) # TODO
 
   # LADs
+  expect_equal(nrow(fetch_lads(c("2017", "2019", "2020", "2021", "2022", "2023", "2024"))), 408)
+  # expect_equal(nrow(fetch_lads(c("2024", "2023"))), 400) # TODO
 
   # LAs
+  #  expect_equal(nrow(fetch_las(c("2017", "2019", "2020", "2021", "2022", "2023", "2024"))), 399) # TODO
+  # expect_equal(nrow(fetch_las(c("2024", "2023"))), 399) # TODO
 })
 
 test_that("2024 locations match what we expect", {
   # PCONs
+  # https://www.parliament.uk/about/how/elections-and-voting/constituencies/
   expect_equal(nrow(fetch_pcons("2024")), 650)
   expect_equal(nrow(fetch_pcons("2024", c("England", "Scotland"))), 600)
   expect_equal(nrow(fetch_pcons("2024", "England")), 543)
@@ -33,28 +40,19 @@ test_that("2024 locations match what we expect", {
   expect_equal(nrow(fetch_pcons("2024", "Wales")), 32)
   expect_equal(nrow(fetch_pcons("2024", "Northern Ireland")), 18)
 
-  # # LADs
-  # expect_equal(nrow(fetch_lads("2024")), 361)
-  # expect_equal(nrow(fetch_lads("2024", c("England", "Wales"))), 328)
-  # expect_equal(nrow(fetch_lads("2024", "England")), 296)
-  # expect_equal(nrow(fetch_lads("2024", "Scotland")), 32)
-  # expect_equal(nrow(fetch_lads("2024", "Wales")), 22)
-  # expect_equal(nrow(fetch_lads("2024", "Northern Ireland")), 11)
-  #
-  # # LAs
-  # expect_equal(nrow(fetch_las("2024")), 220)
-  # expect_equal(nrow(fetch_las("2024", c("England", "Scotland", "Wales"))), 209)
-  # expect_equal(nrow(fetch_las("2024", "England")), 153)
-  # expect_equal(nrow(fetch_las("2024", "Scotland")), 32)
-  # expect_equal(nrow(fetch_las("2024", "Wales")), 22)
-  # expect_equal(nrow(fetch_las("2024", "Northern Ireland")), 11)
+  # LADs
+  expect_equal(nrow(fetch_lads("2024")), 361)
+  expect_equal(nrow(fetch_lads("2024", c("England", "Wales"))), 318)
+  expect_equal(nrow(fetch_lads("2024", "England")), 296)
+  expect_equal(nrow(fetch_lads("2024", "Scotland")), 32)
+  expect_equal(nrow(fetch_lads("2024", "Wales")), 22)
+  expect_equal(nrow(fetch_lads("2024", "Northern Ireland")), 11)
+
+  # LAs
+  #  expect_equal(nrow(fetch_las("2024")), 220) # TODO
+  # expect_equal(nrow(fetch_las("2024", c("England", "Scotland", "Wales"))), 207) # TODO
+  #  expect_equal(nrow(fetch_las("2024", "England")), 153) # TODO
+  #  expect_equal(nrow(fetch_las("2024", "Scotland")), 32) # TODO
+  expect_equal(nrow(fetch_las("2024", "Wales")), 22)
+  expect_equal(nrow(fetch_las("2024", "Northern Ireland")), 11)
 })
-
-# Comparison against existing screener lookup (add to PR notes)
-
-screener <- read.csv("https://raw.githubusercontent.com/dfe-analytical-services/dfe-published-data-qa/main/data/ward_lad_la_pcon_hierarchy.csv")
-
-waldo::compare(
-  screener,
-  dfeR::wd_pcon_lad_la
-)
--- tests/testthat/test-geog_datasets.R
+++ tests/testthat/test-geog_datasets.R
@@ -41,7 +41,7 @@ test_that("code cols are always a 9 digit code", {
   code_columns <-
     tidyselect::ends_with("_code", vars = colnames(dfeR::wd_pcon_lad_la))
 
-  # Check the format for each column
+  # Check the format for each code column
   for (col in code_columns) {
     expect_true(
       all(grepl("^[A-Z]\\d{8}$", dfeR::wd_pcon_lad_la[[col]]))
@@ -52,6 +52,6 @@ test_that("code cols are always a 9 digit code", {
 test_that("rows and cols match description", {
   # This test is more of a reminder when updating the dataset, if these change
   # then we need to update the description in R/geog_datasets.R
-  expect_equal(nrow(dfeR::wd_pcon_lad_la), 11691)
+  expect_equal(nrow(dfeR::wd_pcon_lad_la), 24629)
   expect_equal(ncol(dfeR::wd_pcon_lad_la), 10)
 })
