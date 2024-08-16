4e12f6df00db8962c72f9793e807405bc34a4663
Cam Race <cameron.race@education.gov.uk>
2024-08-16 12:06:25 +0100

    Everything finally works!

--- R/fetch_geographies.R
+++ R/fetch_geographies.R
@@ -4,7 +4,7 @@
 #' given year and country based on the dfeR::wd_pcon_lad_la file
 #'
 #' @param year year to filter the locations to, default is "All",
-#' options of "2017", "2019", "2020", "2021", "2022", "2023", "2024"
+#' options of 2017, 2019, 2020, 2021, 2022", 2023, 2024
 #' @param countries vector of desired countries to filter the locations to,
 #' default is "All", or can be a vector with options of "England", "Scotland",
 #' "Wales" or "Northern Ireland"
@@ -14,68 +14,32 @@
 #'
 #' @name fetch
 #' @examples
-#' fetch_pcons()
-#' fetch_pcons("2023")
-#' fetch_pcons(countries = "Scotland")
-#' fetch_pcons(year = "2023", countries = c("England", "Wales"))
 #'
-#' fetch_las("2024", "Wales")
+#' # Using head() to show only top 5 rows for examples
+#' head(fetch_pcons())
 #'
-#' fetch_pcons("2022", "Northern Ireland")
-
-# TODO: prevent the copy and pasting in this script and break out a separate common function to pump stuff into
+#' head(fetch_pcons(2023))
+#'
+#' head(fetch_pcons(countries = "Scotland"))
+#'
+#' head(fetch_pcons(year = 2023, countries = c("England", "Wales")))
+#'
+#' fetch_lads(2024, "Wales")
+#'
+#' fetch_las(2022, "Northern Ireland")
 fetch_pcons <- function(year = "All", countries = "All") {
-  # Function to check the inputs are valid
+  # Helper function to check the inputs are valid
   check_fetch_location_inputs(year, countries)
 
-  # Filter the lookup to the columns we care about
-  pcon_cols <- c(
-    "pcon_code", "pcon_name", "first_available_year_included",
-    "most_recent_year_included"
+  # Helper function to filter to locations we want
+  output <- fetch_locations(
+    lookup_data = dfeR::wd_pcon_lad_la,
+    cols = c("pcon_code", "pcon_name"),
+    year = year,
+    countries = countries
   )
 
-  lookup <- dplyr::select(dfeR::wd_pcon_lad_la, dplyr::all_of(pcon_cols))
-
-  resummarised_lookup <- lookup %>%
-    dplyr::summarise(
-      "first_available_year_included" =
-        min(.data$first_available_year_included),
-      "most_recent_year_included" =
-        max(.data$most_recent_year_included),
-      .by = c("pcon_name", "pcon_code")
-    )
-
-  # Return early without filtering if defaults are used
-  if (all(year == "All", countries == "All")) {
-    # Drop the time cols
-    return(dplyr::distinct(resummarised_lookup[, 1:2]))
-  }
-
-  # Filtering based on years and countries
-  if (year != "All") {
-    resummarised_lookup <-
-      with(
-        resummarised_lookup,
-        subset(resummarised_lookup, most_recent_year_included == year)
-      )
-  }
-  if (paste0(countries, collapse = "") != "All") {
-    # Filter every value to its first letter as that matches the ONS code
-    # then filter the data set to only have codes from the selected countries
-    resummarised_lookup <- with(
-      resummarised_lookup,
-      subset(
-        resummarised_lookup,
-        grepl(
-          paste0("^", unique(substr(countries, 1, 1)), collapse = "|"),
-          pcon_code
-        )
-      )
-    )
-  }
-
-  # Drop the time cols
-  return(dplyr::distinct(resummarised_lookup[, 1:2]))
+  return(output)
 }
 
 #' Fetch local authority districts
@@ -88,57 +52,18 @@ fetch_pcons <- function(year = "All", countries = "All") {
 #'
 #' @inherit fetch examples
 fetch_lads <- function(year = "All", countries = "All") {
-  # Function to check the inputs are valid
+  # Helper function to check the inputs are valid
   check_fetch_location_inputs(year, countries)
 
-  # Filter the lookup to the columns we care about
-  lad_cols <- c(
-    "lad_code", "lad_name", "first_available_year_included",
-    "most_recent_year_included"
+  # Helper function to filter to locations we want
+  output <- fetch_locations(
+    lookup_data = dfeR::wd_pcon_lad_la,
+    cols = c("lad_code", "lad_name"),
+    year = year,
+    countries = countries
   )
 
-  lookup <- dplyr::select(dfeR::wd_pcon_lad_la, dplyr::all_of(lad_cols))
-
-  resummarised_lookup <- lookup %>%
-    dplyr::summarise(
-      "first_available_year_included" =
-        min(.data$first_available_year_included),
-      "most_recent_year_included" =
-        max(.data$most_recent_year_included),
-      .by = c("lad_name", "lad_code")
-    )
-
-  # Return early without filtering if defaults are used
-  if (all(year == "All", countries == "All")) {
-    # Drop the time cols
-    return(dplyr::distinct(resummarised_lookup[, 1:2]))
-  }
-
-  # Filtering based on years and countries
-  if (year != "All") {
-    resummarised_lookup <-
-      with(
-        resummarised_lookup,
-        subset(resummarised_lookup, most_recent_year_included == year)
-      )
-  }
-  if (paste0(countries, collapse = "") != "All") {
-    # Filter every value to its first letter as that matches the ONS code
-    # then filter the data set to only have codes from the selected countries
-    resummarised_lookup <- with(
-      resummarised_lookup,
-      subset(
-        resummarised_lookup,
-        grepl(
-          paste0("^", unique(substr(countries, 1, 1)), collapse = "|"),
-          lad_code
-        )
-      )
-    )
-  }
-
-  # Drop the time cols
-  return(dplyr::distinct(resummarised_lookup[, 1:2]))
+  return(output)
 }
 
 #' Fetch local authorities
@@ -151,56 +76,16 @@ fetch_lads <- function(year = "All", countries = "All") {
 #'
 #' @inherit fetch examples
 fetch_las <- function(year = "All", countries = "All") {
-  # Function to check the inputs are valid
+  # Helper function to check the inputs are valid
   check_fetch_location_inputs(year, countries)
 
-  # Filter the lookup to the columns we care about
-  la_cols <- c(
-    "new_la_code", "la_name", "first_available_year_included",
-    "most_recent_year_included"
+  # Helper function to filter to locations we want
+  output <- fetch_locations(
+    lookup_data = dfeR::wd_pcon_lad_la,
+    cols = c("new_la_code", "la_name"),
+    year = year,
+    countries = countries
   )
 
-  lookup <- dplyr::select(dfeR::wd_pcon_lad_la, dplyr::all_of(la_cols))
-
-  resummarised_lookup <- lookup %>%
-    dplyr::summarise(
-      "first_available_year_included" =
-        min(.data$first_available_year_included),
-      "most_recent_year_included" =
-        max(.data$most_recent_year_included),
-      .by = c("la_name", "new_la_code")
-    )
-
-  # Return early without filtering if defaults are used
-  if (all(year == "All", countries == "All")) {
-    # Drop the time cols
-    return(dplyr::distinct(resummarised_lookup[, 1:2]))
-  }
-
-  # Filtering based on years and countries
-  if (year != "All") {
-    # TODO: Make this do it fancier to actually get the year!
-    resummarised_lookup <-
-      with(
-        resummarised_lookup,
-        subset(resummarised_lookup, most_recent_year_included == year)
-      )
-  }
-  if (paste0(countries, collapse = "") != "All") {
-    # Filter every value to its first letter as that matches the ONS code
-    # then filter the data set to only have codes from the selected countries
-    resummarised_lookup <- with(
-      resummarised_lookup,
-      subset(
-        resummarised_lookup,
-        grepl(
-          paste0("^", unique(substr(countries, 1, 1)), collapse = "|"),
-          new_la_code
-        )
-      )
-    )
-  }
-
-  # Drop the time cols
-  return(dplyr::distinct(resummarised_lookup[, 1:2]))
+  return(output)
 }
--- R/internal_functions.R
+++ R/internal_functions.R
@@ -29,6 +29,98 @@ check_fetch_location_inputs <- function(year_input, country_input) {
   }
 }
 
+#' Fetch locations for a given lookup
+#'
+#' Helper function for the fetch_xxx() functions to save repeating code
+#'
+#' @param lookup_data lookup data to use to extract locations from
+#' @param cols columns to extract from the main lookup table
+#' @param year year of locations to extract, "All" will skip any filtering and
+#' return all possible locations
+#' @param countries countries for locations to be take from, "All" will skip
+#' any filtering and return all
+#'
+#' @return a data frame of location names and codes
+#' @keywords internal
+fetch_locations <- function(lookup_data, cols, year, countries) {
+  # Return only the cols we specified
+  # We know their position from the dplyr selection of the lookup
+  # This is used wherever this function returns an output
+  cols_to_return <- seq_along(cols)
+
+  # Pull in main lookup data
+  lookup <- dplyr::select(
+    lookup_data,
+    dplyr::all_of(
+      c(cols, "first_available_year_included", "most_recent_year_included")
+    )
+  )
+
+  # Resummarise the years to each unique location
+  resummarised_lookup <- lookup %>%
+    dplyr::summarise(
+      "first_available_year_included" =
+        min(.data$first_available_year_included),
+      "most_recent_year_included" =
+        max(.data$most_recent_year_included),
+      .by = dplyr::all_of(cols)
+    )
+
+  # Return early without filtering if defaults are used
+  if (all(year == "All", countries == "All")) {
+    return(dplyr::distinct(resummarised_lookup[, cols_to_return]))
+  }
+
+  # Filter based on year selection if specified
+  if (year != "All") {
+    # Flag the rows that are in the year asked for
+    resummarised_lookup <- resummarised_lookup %>%
+      dplyr::mutate("in_specified_year" = ifelse(
+        as.numeric(.data$most_recent_year_included) >= year &
+          as.numeric(.data$first_available_year_included) <= year,
+        TRUE,
+        FALSE
+      ))
+
+    # Filter to only those locations
+    resummarised_lookup <- with(
+      resummarised_lookup,
+      subset(resummarised_lookup, in_specified_year == TRUE)
+    ) %>%
+      dplyr::select(-c("in_specified_year")) # remove temp column
+  }
+
+  # Filter based on country selcetion if specified
+  if (paste0(countries, collapse = "") != "All") {
+    # Get the code column
+    # Take new_la_code if present (as sometimes there may also be old_la code)
+    # Otherwise work it out
+    if ("new_la_code" %in% cols) {
+      code_col <- "new_la_code"
+    } else {
+      code_col <- grep("_code$", cols, value = TRUE)
+    }
+
+    if (length(code_col) != 1) {
+      stop(
+        "More than one code column found, there must only be one code column"
+      )
+    }
+
+    # Filter every value to its first letter as that matches the ONS code
+    # then filter the data set to only have codes from the selected countries
+    resummarised_lookup <- resummarised_lookup %>%
+      dplyr::filter(
+        grepl(
+          paste0("^", unique(substr(countries, 1, 1)), collapse = "|"),
+          !!rlang::sym(code_col)
+        )
+      )
+  }
+
+  return(dplyr::distinct(resummarised_lookup[, cols_to_return]))
+}
+
 #' Tidy a lookup file from the Open Geography Portal
 #'
 #' Takes a file from the open geography portal and tidies it ready for
