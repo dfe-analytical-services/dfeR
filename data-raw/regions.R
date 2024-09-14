e8f518422c4edadbbc41f0c22991f4546ee5e3f8
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-14 10:17:56 +0100

    Update countries and regions to use year variables at start of scripts

--- data-raw/regions.R
+++ data-raw/regions.R
@@ -1,21 +1,33 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Get a list of regions and codes from ONS API
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Set the year ----------------------------------------------------------------
+year <- 2023
+year_end <- 23
 
+test_that("`year` ends in `year_end`", {
+  expect_true(grepl(paste0(year_end, "$"), year))
+})
+
+# Set query parameters --------------------------------------------------------
 params <- list(
   where = "1=1",
-  outFields = "RGN23CD,RGN23NM",
+  outFields = paste0("RGN", year_end, "CD,RGN", year_end, "NM"),
   outSR = 4326,
   f = "json"
 )
 
+# Run query -------------------------------------------------------------------
 ons_regions <- dfeR::get_ons_api_data(
-  "RGN_DEC_2023_EN_NC",
+  paste0("RGN_DEC_", year, "_EN_NC"), # data set uses 4 digit year
   query_params = params
 ) |>
   dplyr::rename(
-    "region_name" = attributes.RGN23NM,
-    "region_code" = attributes.RGN23CD
+    "region_name" = paste0("attributes.RGN", year_end, "NM"),
+    "region_code" = paste0("attributes.RGN", year_end, "CD")
   )
 
+# Set custom DfE regions ------------------------------------------------------
 custom_dfe_regions <- data.frame(
   "region_name" = c(
     "Inner London", # This is a county we tend to publish as a region
@@ -29,6 +41,7 @@ custom_dfe_regions <- data.frame(
   "region_code" = c("E13000001", "E13000002", "z", "z", "z", "z", "z")
 )
 
+# Stack into one data frame and write out -------------------------------------
 regions <- rbind(ons_regions, custom_dfe_regions)
 
 usethis::use_data(regions, overwrite = TRUE)
