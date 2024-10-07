a5e72f6e4fbbfdf69b75453cabe4a89ea22a03cc
ZAYED <Menna.ZAYED@EDUCATION.GOV.UK>
2024-10-07 17:01:08 +0100

    fixing formtting issues for lint testing

--- tests/testthat/test-pretty_num.R
+++ tests/testthat/test-pretty_num.R
@@ -10,8 +10,8 @@ test_that("prettifies", {
   expect_equal(pretty_num(11^9, gbp = TRUE, dp = 3), "£2.358 billion")
   expect_equal(pretty_num(-11^8, gbp = TRUE, dp = -1), "-£210 million")
   expect_equal(pretty_num(-123421421), "-123.42 million")
-  expect_equal(pretty_num(63.71, dp=1,nsmall=2), "63.70")
-  expect_equal(pretty_num(894.1, dp=2,nsmall=3), "894.100")
+  expect_equal(pretty_num(63.71, dp = 1, nsmall = 2), "63.70")
+  expect_equal(pretty_num(894.1, dp = 2, nsmall = 3), "894.100")
   expect_equal(
     pretty_num(11^8, prefix = "+/-", gbp = TRUE, dp = -1.00), "+£210 million"
   )
@@ -31,7 +31,7 @@ test_that("tests multiple values", {
   )
 
   expect_equal(
-    pretty_num(c(1:4), nsmall=1),
+    pretty_num(c(1:4), nsmall = 1),
     c("1.0", "2.0", "3.0", "4.0")
   )
 })
