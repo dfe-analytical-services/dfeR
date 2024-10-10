b1cfd371047e9fb8af25ad38eebc21e92851b6d8
ZAYED <Menna.ZAYED@EDUCATION.GOV.UK>
2024-10-10 15:51:53 +0100

    started this function but need more clarification before moving forward

---
+++ R/z_replace.R
@@ -0,0 +1,22 @@
+#' Replaces NA values
+#'
+#' @description
+#' Replaces NA values in data frames except for time and location columns
+#'
+#' @details
+
+#'
+#' @param table_name name of the table that you want to replace NA values in
+#' @param replacement_alt optional - if you want the NA replacement value to be different to "z"
+#'
+#' @return table with "z" instead of NA values
+#' @export
+#' @examples
+
+
+z_replace <- function(table_name,replacement_alt){
+
+
+
+
+}
