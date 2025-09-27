a46652d39ed669410923f40d86f73a74153437f8
cjrace <52536248+cjrace@users.noreply.github.com>
2025-09-27 12:24:32 +0100

    make mayoral snapshot more readable

--- tests/testthat/test-fetch_mayoral.R
+++ tests/testthat/test-fetch_mayoral.R
@@ -10,7 +10,7 @@ test_that("Has no duplicate rows", {
 # Snapshot test outputs
 test_that("location counts per year are consistent", {
   expect_snapshot(
-    sapply(2017:2025, fetch_mayoral)
+    lapply(2017:2025, fetch_mayoral)
   )
 })
 
