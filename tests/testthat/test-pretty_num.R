94841cc77e50f68b482366684f88e1c6dc85e982
menna.zayed@education.gov.uk <menna.zayed@education.gov.uk>
2025-11-20 16:55:48 +0000

    removing test as no longer applicable

--- tests/testthat/test-pretty_num.R
+++ tests/testthat/test-pretty_num.R
@@ -89,10 +89,3 @@ test_that("dynamic_dp works with negative values", {
     "-3.00 billion"
   )
 })
-
-test_that("dynamic_dp is ignored when disabled", {
-  expect_equal(
-    pretty_num(3e9, dp = 1, dynamic_dp_value = 3),
-    "3.0 billion"
-  )
-})
