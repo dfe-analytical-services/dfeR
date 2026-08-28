b2fc0676c02288be3dd5828427542559863eda96
cjrace <52536248+cjrace@users.noreply.github.com>
2026-08-28 10:12:09 +0100

    Break long URL over multiple lines

--- R/check_databricks_odbc.R
+++ R/check_databricks_odbc.R
@@ -71,10 +71,15 @@ check_databricks_odbc <- function() {
       )
     )
     cli::cli_ul(
-      "Please set them in your {.file .Renviron} file
-      or in your Windows account environment variables.
-      Follow the instructions on the linked DfE Analyst guide below: \n
-      {.url https://dfe-analytical-services.github.io/analysts-guide/ADA/databricks_rstudio_sql_warehouse.html#establishing-an-rstudio-connection-using-environment-variables }"
+      paste0(
+        "Please set them in your {.file .Renviron} file
+        or in your Windows account environment variables.
+        Follow the instructions on the linked DfE Analyst guide below: \n
+        {.url ",
+        "https://dfe-analytical-services.github.io/analysts-guide/ADA/",
+        "databricks_rstudio_sql_warehouse.html",
+        "#establishing-an-rstudio-connection-using-environment-variables }"
+      )
     )
     return(invisible(FALSE))
   }
